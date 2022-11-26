7363ac24d558933de05f7f3f500ad2c8e91c7def
Enekourquijo <eneko.urquijo@opendeusto.es>
2022-11-26 10:42:19 +0100

    ultima
    Co-Authored-By: JaimeRevilla <90686026+JaimeRevilla@users.noreply.github.com>
    Co-Authored-By: enriquepuente2 <91028770+enriquepuente2@users.noreply.github.com>
    Co-Authored-By: ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>

--- src/es/deusto/ingenieria/sd/strava/remote/GoogleService.java
+++ src/es/deusto/ingenieria/sd/strava/remote/GoogleService.java
@@ -8,16 +8,20 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GoogleService extends UnicastRemoteObject implements IGoogleServer {
 	private static final long serialVersionUID = 1L;
-
+	private Map<String, String> mapaService;
 	
 	//Attribute for the Singleton pattern
 	public static GoogleService instance;
 			
 	private GoogleService() throws RemoteException {
 		super();
+		mapaService = new HashMap<>();
+		mapaService.put("1@gmail.com", "12345");
 	}
 	
 	public static GoogleService getInstance() {
@@ -32,8 +36,11 @@ public class GoogleService extends UnicastRemoteObject implements IGoogleServer 
 		return instance;
 	}
 
-	public void login(String mail, String contr) throws RemoteException{
+	public boolean login(String mail, String contr) throws RemoteException{
 		System.out.println("Este es el rmi de google!");
-		System.out.println("vkjnvkvnr");
+		if(mapaService.containsKey(mail)) {
+			return contr.equals(mapaService.get(mail));
+		}
+		return false;
 	}
 }
